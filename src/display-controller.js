e8beeed2f2acf549c9bc119b1497bbb07224990c
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-09-09 19:00:38 +0530

    improved style for main divs, added template text to check how titles are loaded

--- src/display-controller.js
+++ src/display-controller.js
@@ -15,6 +15,7 @@ const divHeading = (title, appendTo) => {
 const divTemplate = (id, appendTo, title) => {
   const div = document.createElement('div')
   div.id = id
+  div.className = 'div-style'
   document.querySelector(`#${appendTo}`).append(div)
   divHeading(title, `#${id}`)
 }
@@ -24,12 +25,30 @@ const container = () => {
   div.id = 'container'
   document.querySelector('#content').append(div)
   divTemplate('project-div', 'container', 'Projects')
+  projectContents()
   divTemplate('todo-div', 'container', 'TODOs')
+}
+
+const projectContents = () => {
+  const projects = ['Project - 1', 'Project - 2']
+  const div = document.createElement('div')
+  div.id = 'project-content'
+  document.querySelector('#project-div').append(div)
+  projects.forEach(project => {
+    const temp = document.createElement('div')
+    temp.classList.add('project-title')
+    temp.textContent = project
+    document.querySelector('#project-content').append(temp)
+  })
+}
+const newProjectButton = () => {
+
 }
 
 const printLayout = () => {
   titleBar("Clumsyknight's TODO List")
   container()
+
 }
 
 export { printLayout }
